23b84e9884a72997c497b1a1a52d0b7fbb8b7a19
Leon <blackcat1203@gmail.com>
2014-08-16 13:54:28 +0800

    Hello World

--- SwiftApp/ViewController.swift
+++ SwiftApp/ViewController.swift
@@ -9,10 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-                            
+    
+    var name:String="Hello World"
+    
+    @IBOutlet var label:UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        label.text=name;
     }
 
     override func didReceiveMemoryWarning() {
