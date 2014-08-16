767d542f53a4e59179804cc3e04d99a4f2be070a
Leon <blackcat1203@gmail.com>
2014-08-16 15:31:55 +0800

    查天氣程式

--- SwiftApp/ViewController.swift
+++ SwiftApp/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController ,NSURLConnectionDataDelegate{
     
     var name:String="Hello World"
     
@@ -19,14 +19,29 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.label.text=name;
-        self.image.image=UIImage(named: "rain.png")
+//        self.image.image=UIImage(named: "rain.png")
+        //png 可以忽略 官方建議png
+        let background = UIImage(named: "rainbackground.jpg")
+        self.view.backgroundColor = UIColor(patternImage: background)
+        
+        var restAPI:String = "http://api.openweathermap.org/data/2.5/weather?q=Taipei"
+        var url :NSURL = NSURL(string:restAPI)
+        var request:NSURLRequest = NSURLRequest(URL:url)
+        var connection: NSURLConnection = NSURLConnection(request: request, delegate: self,startImmediately:true)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func connection(connection: NSURLConnection!, didReceiveData data: NSData!) {
+        println("connection");
+    }
+    
+    func connectionDidFinishLoading(connection: NSURLConnection!) {
+        println("did");
+    }
 
 }
 
